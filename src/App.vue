3796a92ec4056a29a4a1982c1f432493901e5e11
Eugen Istoc <eugenistoc@gmail.com>
2020-02-18 01:01:55 -0500

    Create simple chat screen

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h2>Chattty!</h2>
+    <MessagesList />
+    <input type="text" placeholder="Enter Messsage" />
+    <button>Send</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MessagesList from "./components/MessagesList";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    MessagesList
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/MessagesList.vue
@@ -0,0 +1,7 @@
+<template>
+  <ul>
+    <li>message 1</li>
+    <li>message 2</li>
+    <li>message 3</li>
+  </ul>
+</template>
